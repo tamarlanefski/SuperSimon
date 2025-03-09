ec391b2edc31d5dfc0cfaf6fd339a3babc31ff08
tamarlanefski <a0548481846@gmail.com>
2025-03-09 19:42:07 +0200

    users_update

--- src/components/UserName/userName.tsx
+++ src/components/UserName/userName.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { BsPersonCircle } from "react-icons/bs";
